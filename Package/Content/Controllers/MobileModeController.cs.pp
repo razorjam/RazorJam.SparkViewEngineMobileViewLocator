0b9459bce9e2d6cbdf8aa2c638d03ded6b21ecb2
Jamie Hinton <jamie.hinton@curlyweb.co.uk>
2012-08-09 20:54:00 +0100

    added package

---
+++ Package/Content/Controllers/MobileModeController.cs.pp
@@ -0,0 +1,33 @@
+﻿namespace $rootnamespace$.Controllers
+{
+  using System.Web.Mvc;
+  using CurlyWeb.SparkViewLocator.ViewHelper.Interfaces;
+
+  public class MobileModeController : Controller
+  {
+    private readonly IMobileCookieHelper mobileCookieHelper;
+
+    public MobileModeController( IMobileCookieHelper mobileCookieHelper )
+    {
+      this.mobileCookieHelper = mobileCookieHelper;
+    }
+
+    public RedirectResult Desktop()
+    {
+      mobileCookieHelper.ForceDesktopSite();
+      return Redirect( RedirectTo() );
+    }
+
+    private string RedirectTo()
+    {
+      var redirectTo = HttpContext.Request.UrlReferrer == null ? "~/" : HttpContext.Request.UrlReferrer.LocalPath;
+      return redirectTo;
+    }
+
+    public RedirectResult Mobile()
+    {
+      mobileCookieHelper.ForceViewMobileSite();
+      return Redirect( RedirectTo() );
+    }
+  }
+}
